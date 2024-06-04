30a3df71b0962c83ad791527b0df637a76f6fa5f
Kiran K <kiran@dub.co>
2024-06-04 18:31:42 +0530

    add remaining routes

--- node/express/index.ts
+++ node/express/index.ts
@@ -4,10 +4,55 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+// Create a link
 app.get("/create-link", async (req: Request, res: Response) => {
   try {
     const result = await dub.links.create({
       url: "https://www.google.com",
+    });
+
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(400).json(error);
+  }
+});
+
+// Upsert a link
+// Update the link if same URL already exists or create a new link
+app.get("/upsert-link", async (req: Request, res: Response) => {
+  try {
+    const result = await dub.links.upsert({
+      url: "https://www.google.com",
+      externalId: "my-link-id",
+    });
+
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(400).json(error);
+  }
+});
+
+// Update a link
+app.get("/update-link", async (req: Request, res: Response) => {
+  try {
+    const result = await dub.links.create({
+      url: "https://www.google.us",
+    });
+
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(400).json(error);
+  }
+});
+
+// Retrieve analytics for link
+// See the API reference to see all the options available https://dub.co/docs/api-reference/endpoint/retrieve-analytics
+app.get("/analytics", async (req: Request, res: Response) => {
+  try {
+    const result = await dub.analytics.retrieve({
+      interval: "7d",
+      event: "clicks",
+      groupBy: "timeseries",
     });
 
     res.status(200).json(result);
