501964782a266a4e774c5c7101693bf9c599ace7
Kiran K <kiran@dub.co>
2024-06-04 17:55:38 +0530

    remove /dist

--- node/express/dist/dub.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// @ts-nocheck
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dub = void 0;
-const dub_1 = require("dub");
-exports.dub = new dub_1.Dub({
-    token: process.env.DUB_API_KEY,
-    workspaceId: process.env.DUB_WORKSPACE_ID,
-});
