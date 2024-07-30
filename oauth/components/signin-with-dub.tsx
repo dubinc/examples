30ccd8291b0d601b68bbdfcadbc4f8d09b791fe6
Steven Tey <stevensteel97@gmail.com>
2024-07-29 23:09:53 -0700

    update to use the proper styles

--- oauth/components/signin-with-dub.tsx
+++ oauth/components/signin-with-dub.tsx
@@ -2,16 +2,22 @@
 
 import { Button } from "@dub/ui";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function SignInWithDub() {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <Button
       variant="primary"
       text="Sign in with Dub"
       className="w-30 h-10 rounded"
-      onClick={() => router.push("/api/oauth/authorize")}
+      loading={isLoading}
+      onClick={() => {
+        setIsLoading(true);
+        router.push("/api/oauth/authorize");
+      }}
     />
   );
 }
--- oauth/tailwind.config.ts
+++ oauth/tailwind.config.ts
@@ -1,20 +1,30 @@
+import sharedConfig from "@dub/tailwind-config/tailwind.config.ts";
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+const config: Pick<Config, "presets"> = {
+  presets: [
+    {
+      ...sharedConfig,
+      content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        // Path to Dub module
+        "./node_modules/@dub/ui/**/*.{mjs,js,ts,jsx,tsx}",
+      ],
+      theme: {
+        extend: {
+          ...sharedConfig?.theme?.extend,
+          backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+              "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          },
+        },
       },
+      plugins: [require("@tailwindcss/typography")],
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
+  ],
 };
+
 export default config;
