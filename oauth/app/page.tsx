b152cfc88f955bf8cd09770f41fc6760ec08457d
Steven Tey <stevensteel97@gmail.com>
2024-08-06 12:28:37 -0700

    fix h1

--- oauth/app/page.tsx
+++ oauth/app/page.tsx
@@ -10,8 +10,12 @@ export default async function Home() {
       <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="px-4 md:px-6 flex flex-col items-center text-center space-y-6">
           <div className="max-w-4xl">
-            <h1 className="font-display text-5xl font-bold text-black max-w-xl mx-auto">
-              Kickstart your Integration with Dub
+            <h1 className="mt-5 font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]">
+              Kickstart your
+              <br />
+              <span className="bg-gradient-to-r py-1 from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">
+                Dub Integration
+              </span>
             </h1>
             <p className="mt-6 text-lg text-gray-500">
               Integrations allow you to extend the capabilities of Dub and
