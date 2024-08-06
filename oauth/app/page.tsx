b125c7a76e674b6743c981527dfb5c0324dc10ad
Tim Wilson <tim@twilson.net>
2024-08-06 16:31:10 -0400

    Update page.tsx

--- oauth/app/page.tsx
+++ oauth/app/page.tsx
@@ -2,16 +2,23 @@ import { LinksDemo } from "@/components/links-demo";
 import SignInWithDub from "@/components/signin-with-dub";
 import { getSession } from "@/lib/actions";
 import { ClientOnly } from "@dub/ui";
+import { cn } from "@dub/utils";
 
 export default async function Home() {
   const session = await getSession();
 
   return (
-    <main className="container mx-auto max-w-4xl">
-      <section className="mt-12 md:mt-16 w-full py-12">
+    <main
+      className={cn(
+        "container mx-auto max-w-4xl",
+        !session?.user &&
+          "min-h-screen flex items-center flex-col justify-center"
+      )}
+    >
+      <section className="w-full py-24 md:py-32">
         <div className="px-4 md:px-6 flex flex-col items-center text-center space-y-6">
           <div className="max-w-4xl">
-            <h1 className="mt-5 font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]">
+            <h1 className="font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]">
               Kickstart your
               <br />
               <span className="bg-gradient-to-r py-1 from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">
