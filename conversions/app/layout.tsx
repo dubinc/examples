74811a1464b14343c1634414af8fb9ff29dce7ff
Kiran K <kiran@dub.co>
2024-08-15 00:18:45 +0530

    install @dub/analytics

--- conversions/app/layout.tsx
+++ conversions/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics as DubAnalytics } from "@dub/analytics/react";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark:bg-slate-950">
       <body className={inter.className}>{children}</body>
+      <DubAnalytics />
     </html>
   );
 }
