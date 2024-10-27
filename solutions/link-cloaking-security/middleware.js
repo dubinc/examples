64e00acf3ae59b8c2ba37617859e598e07862ca0
Steven Tey <stevensteel97@gmail.com>
2024-10-27 13:53:57 -0700

    remove x-frame-options

--- solutions/link-cloaking-security/middleware.js
+++ solutions/link-cloaking-security/middleware.js
@@ -3,7 +3,6 @@ import { next } from "@vercel/edge";
 export default function middleware() {
   return next({
     headers: {
-      "X-Frame-Options": "ALLOW-FROM dub.link",
       "Content-Security-Policy": "frame-ancestors 'self' dub.link",
       "Strict-Transport-Security":
         "max-age=31536000; includeSubDomains; preload",
