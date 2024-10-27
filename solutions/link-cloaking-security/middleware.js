387c4e668801fec5c3700d9ed7f25ad193c47a27
Steven Tey <stevensteel97@gmail.com>
2024-10-27 13:56:36 -0700

    DENY

--- solutions/link-cloaking-security/middleware.js
+++ solutions/link-cloaking-security/middleware.js
@@ -3,6 +3,7 @@ import { next } from "@vercel/edge";
 export default function middleware() {
   return next({
     headers: {
+      "X-Frame-Options": "DENY",
       "Content-Security-Policy": "frame-ancestors 'self' dub.link",
       "Strict-Transport-Security":
         "max-age=31536000; includeSubDomains; preload",
