58d41688ee45c854922baf19401adf1e7a7d116e
Kiran K <kiran@dub.co>
2025-04-29 23:53:49 +0530

    @dub/embed-react

---
+++ partners/whitelabeling/app/dashboard/page-client.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { DubEmbed } from "@dub/embed-react";
+
+export function PageClient() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const [publicToken, setPublicToken] = useState("");
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status, router]);
+
+  useEffect(() => {
+    const fetchPublicToken = async () => {
+      const response = await fetch("/api/embed-token"); // TODO: add this endpoint
+      const data = await response.json();
+
+      setPublicToken(data.publicToken);
+    };
+
+    fetchPublicToken();
+  }, []);
+
+  if (!publicToken) {
+    return <div>Loading...</div>;
+  }
+
+  return <DubEmbed data="referrals" token={publicToken} />;
+}
