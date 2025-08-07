25ae0996a80a5ac0a77026942022a48bb2adadff
Steven Tey <stevensteel97@gmail.com>
2025-08-07 15:36:56 -0700

    draggable=false

--- embed/referrals/app/page.tsx
+++ embed/referrals/app/page.tsx
@@ -29,8 +29,20 @@ export default async function Dashboard({
       <div className="absolute inset-0 blur-sm opacity-90">
         <div className="absolute inset-x-4 top-0 bottom-4 rounded-xl bg-foreground/5 overflow-hidden">
           <div className="absolute left-1/2 w-full max-w-screen-lg top-0 aspect-[2/3] -translate-x-1/2 sm:block hidden">
-            <Image src="/screenshots/full-light.jpg" alt="Screenshot" fill className="object-contain object-top dark:hidden" />
-            <Image src="/screenshots/full-dark.jpg" alt="Screenshot" fill className="object-contain object-top dark:block hidden" />
+            <Image
+              src="/screenshots/full-light.jpg"
+              draggable={false}
+              alt="Screenshot"
+              fill
+              className="object-contain object-top dark:hidden"
+            />
+            <Image
+              src="/screenshots/full-dark.jpg"
+              draggable={false}
+              alt="Screenshot"
+              fill
+              className="object-contain object-top dark:block hidden"
+            />
           </div>
         </div>
       </div>
@@ -44,8 +56,20 @@ export default async function Dashboard({
           <div className="absolute left-1/2 bottom-0 -translate-x-1/2 w-full max-w-[min(320px,90%)] bg-background h-[85%] rounded-t-lg border-x border-t border-foreground/10">
             <div className="absolute inset-x-1 top-1 bottom-0 overflow-hidden rounded-t-lg">
               {/* Screenshots (light/dark) */}
-              <Image src="/screenshots/small-light.jpg" alt="Screenshot" fill className="object-cover object-top dark:hidden" />
-              <Image src="/screenshots/small-dark.jpg" alt="Screenshot" fill className="object-cover object-top hidden dark:block" />
+              <Image
+                src="/screenshots/small-light.jpg"
+                draggable={false}
+                alt="Screenshot"
+                fill
+                className="object-cover object-top dark:hidden"
+              />
+              <Image
+                src="/screenshots/small-dark.jpg"
+                draggable={false}
+                alt="Screenshot"
+                fill
+                className="object-cover object-top hidden dark:block"
+              />
             </div>
           </div>
         </div>
@@ -54,8 +78,9 @@ export default async function Dashboard({
             Dub Partners Referrals Embed Demo
           </h1>
           <p className="mt-2 text-foreground/70 text-sm leading-normal">
-            Explore the Dub Partners demo. Click the event triggers, and explore the different tabs
-            to see how Dub can help convert your users into advocates effortlessly within your app.
+            Explore the Dub Partners demo. Click the event triggers, and explore
+            the different tabs to see how Dub can help convert your users into
+            advocates effortlessly within your app.
           </p>
           <div className="mt-8">
             <SignupButton />
